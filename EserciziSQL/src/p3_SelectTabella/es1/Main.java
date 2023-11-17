41d5e8e2aa79372b7d49e056b58dfce68f2d25cf
Valeria Parise <parisevaleria@live.com>
2023-11-17 16:58:49 +0100

    es country

--- EserciziSQL/src/p3_SelectTabella/es1/Main.java
+++ EserciziSQL/src/p3_SelectTabella/es1/Main.java
@@ -11,11 +11,10 @@ public class Main {
         // Per connetterci al DB
         Connection connection = null;
         try{
-             connection = DriverManager.getConnection("jdbc:mysql://localhost:3306/es2_newdb", "developer", "dev.1234");
-             Statement statement = connection.createStatement();
-
-
+            connection = DriverManager.getConnection("jdbc:mysql://localhost:3306/es2_newdb", "developer", "dev.1234");
+            Statement statement = connection.createStatement();
             ResultSet resultSet = statement.executeQuery("select first_name, last_name from students");
+
             while(resultSet.next()){
                 System.out.println(resultSet.getString("first_name"));
                 cognome.add(resultSet.getString("last_name"));
--- EserciziSQL/src/p3_SelectTabella/es2/main.java
+++ EserciziSQL/src/p3_SelectTabella/es2/main.java
@@ -1,23 +1,42 @@
 package p3_SelectTabella.es2;
 
-import java.sql.Connection;
-import java.sql.DriverManager;
-import java.sql.ResultSet;
-import java.sql.Statement;
+import java.sql.*;
 import java.util.ArrayList;
 
 public class main {
     public static void main(String[] args) {
+        Connection connection = null;
         try {
-            Connection connection = DriverManager.getConnection("jdbc:mysql://localhost:3306/es3_newdb", "developer",
+            connection = DriverManager.getConnection("jdbc:mysql://localhost:3306/es2_newdb", "developer",
                     "dev.1234");
             Statement statement = connection.createStatement();
-            ResultSet resultSet = statement.executeQuery("select * from students");
+
+            String createCountry = "ALTER TABLE students " + "ADD country varchar(30)";
+
+            String setCountry1 = "UPDATE `es2_newdb`.`students` SET `country`='Italy' WHERE `id_student`=1;" ;
+            String setCountry2 = "UPDATE `es2_newdb`.`students` SET `country`='Italy' WHERE `id_student`=2;" ;
+            String setCountry3 = "UPDATE `es2_newdb`.`students` SET `country`='Germany' WHERE `id_student`=3;";
+            String setCountry4 = "UPDATE `es2_newdb`.`students` SET `country`='Germany' WHERE `id_student`=4;" ;
+            statement.executeUpdate(createCountry);
+            statement.executeUpdate(setCountry1);
+            statement.executeUpdate(setCountry2);
+            statement.executeUpdate(setCountry3);
+            statement.executeUpdate(setCountry4);
+
+            ResultSet resultSet = statement.executeQuery("select first_name, country from students");
+
             while (resultSet.next()) {
-                System.out.println(resultSet.getString("name") + " - " + resultSet.getString("country"));
+                System.out.println(resultSet.getString("first_name") + " - " + resultSet.getString("country"));
             }
-        } catch (Exception e) {
-            e.printStackTrace();
+        } catch (SQLException e ){
+            System.out.println((e.getMessage()));
+        }finally {
+            try {
+                if (connection != null)
+                {connection.close();}
+            }catch (SQLException ex){
+                System.out.println(ex.getMessage());
+            }
         }
     }
 }
