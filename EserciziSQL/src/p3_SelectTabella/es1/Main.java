6abe5cf1e32fefd46e8931aae00d4ad9fd5d8a6b
Valeria Parise <parisevaleria@live.com>
2023-11-17 16:43:57 +0100

    es 1 select

--- EserciziSQL/src/p3_SelectTabella/es1/Main.java
+++ EserciziSQL/src/p3_SelectTabella/es1/Main.java
@@ -2,25 +2,35 @@ package p3_SelectTabella.es1;
 
 import java.sql.*;
 import java.util.ArrayList;
-import java.util.Arrays;
+
 
 public class Main {
     public static void main(String[] args) {
+
+        ArrayList<String> cognome = new ArrayList<>();
         // Per connetterci al DB
+        Connection connection = null;
         try{
-            ArrayList<String> surnames = new ArrayList<>();
-            Connection connection = DriverManager.getConnection("jdbc:mysql://localhost:3306/es3_newdb", "developer",
-                    "dev.1234");
-            Statement statement = connection.createStatement();
-            ResultSet resultSet = statement.executeQuery("select * from students");
+             connection = DriverManager.getConnection("jdbc:mysql://localhost:3306/es2_newdb", "developer", "dev.1234");
+             Statement statement = connection.createStatement();
+
+
+            ResultSet resultSet = statement.executeQuery("select first_name, last_name from students");
             while(resultSet.next()){
-                System.out.println(resultSet.getString("name"));
-                surnames.add(resultSet.getString("surname"));
+                System.out.println(resultSet.getString("first_name"));
+                cognome.add(resultSet.getString("last_name"));
             }
-            System.out.println("Surnames:\n" + surnames);
+            System.out.println(cognome);
         }
-        catch (Exception e ){
-            e.printStackTrace();
+        catch (SQLException e ){
+            System.out.println((e.getMessage()));
+        }finally {
+            try {
+                if (connection != null)
+                {connection.close();}
+            }catch (SQLException ex){
+                System.out.println(ex.getMessage());
+            }
         }
     }
 }
